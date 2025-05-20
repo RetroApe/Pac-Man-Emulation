333902ab28690b6c7e935f6abd550ff75d2e68f6
Tomislav Šuto <113673335+RetroApe@users.noreply.github.com>
2025-05-20 21:23:00 +0200

    Start pause
    I added a brief pause at the start of the game, as is in the original, to better adjust the necessary mechanics and behaviours of ghosts

--- game.gd
+++ game.gd
@@ -2,11 +2,19 @@ extends Node2D
 
 @onready var pac_man: PacMan = %PacMan
 @onready var ghosts: Ghosts = %Ghosts
+@onready var start_timer: Timer = %StartTimer
 
 const GRID = preload("res://resources/Grid.tres")
 const CELL = preload("res://cell.tscn")
 
 func _ready() -> void:
+	start_timer.timeout.connect(func() -> void:
+		get_tree().paused = false
+	)
+	
+	get_tree().paused = true
+	
+	
 	for cell_coord in Ghost.WALKABLE_GHOST_HOUSE:
 		var cell_panel : Panel = CELL.instantiate()
 		cell_panel.position = GRID.calculate_cell_position(cell_coord, true) - Vector2(4.0, 4.0)
