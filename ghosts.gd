22e860e0416721e05e04e51ba959b521a4392b2c
Tomislav Šuto <113673335+RetroApe@users.noreply.github.com>
2025-05-20 13:35:54 +0200

    Added duplicate Ghost
    Named it Pinky, but still behaves like Blinky.
    Adjusted some behaviour in ghosts.gd, added other...

--- ghosts.gd
+++ ghosts.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 signal pacman_dead
 
-@onready var ghost : Ghost = %Ghost
+@onready var blinky : Ghost = %Blinky
 @onready var scatter_chase_timer: Timer = %ScatterChaseTimer
 
 enum {
@@ -28,29 +28,36 @@ func _ready() -> void:
 	scatter_chase_timer.start(EventBus.scatter_chase_timing[_current_level][0])
 	scatter_chase_timer.timeout.connect(func() -> void:
 		EventBus.scatter_chase_timing_counter += 1
+		
 		_current_state = EventBus.scatter_chase_timing_counter % 2
+		#print(_current_state, " = ", EventBus.scatter_chase_timing_counter)
+		for each in _ghosts_array:
+			if each.is_inside_the_ghost_house == false and each.current_state == each.State.TARGETING:
+				each.switch_direction()
 		var time : float = EventBus.scatter_chase_timing[_current_level][EventBus.scatter_chase_timing_counter]
 		if time != -1.0:
-			scatter_chase_timer.wait_time = time
+			#print("Time: ", time)
+			scatter_chase_timer.start(time)
 		else:
 			scatter_chase_timer.stop()
 	)
 
 func _process(_delta: float) -> void:
 	if (
-		ghost.current_state == ghost.State.TARGETING and 
-		ghost.is_inside_the_ghost_house == false and
+		blinky.current_state == blinky.State.TARGETING and 
+		blinky.is_inside_the_ghost_house == false and
 		_current_state == CHASE
 	):
-		ghost.target_coordinates = pacman_current_cell_coordinates
-	elif _current_state == SCATTER:
-		ghost.target_coordinates = ghost.scatter_coordinates
+		blinky.target_coordinates = pacman_current_cell_coordinates
+	elif _current_state == SCATTER and blinky.current_state != blinky.State.EATEN:
+		blinky.target_coordinates = blinky._scatter_coordinates
 	
-	if ghost.current_cell_coordinates == pacman_current_cell_coordinates:
-		if ghost.current_state == ghost.State.TARGETING:
-			pacman_dead.emit()
-		if ghost.current_state == ghost.State.FRIGHTENED:
-			ghost.death()
+	for each in _ghosts_array:
+		if each.current_cell_coordinates == pacman_current_cell_coordinates:
+			if each.current_state == each.State.TARGETING:
+				pacman_dead.emit()
+			if each.current_state == each.State.FRIGHTENED:
+				each.death()
 
 func frightened() -> void:
 	for ghostee in _ghosts_array:
