d8f8b3bddbbb22ac298b0c71d7ceb19d76c58213
Tomislav Šuto <113673335+RetroApe@users.noreply.github.com>
2025-05-21 09:54:16 +0200

    Scatter-Chase Update
    Timing is now set for all levels

--- ghosts.gd
+++ ghosts.gd
@@ -4,7 +4,8 @@ extends Node2D
 signal pacman_dead
 
 @onready var scatter_chase_timer: Timer = %ScatterChaseTimer
-
+var _scatter_chase_timing : Array
+var _scatter_chase_timing_counter := 0
 enum {
 	SCATTER,
 	CHASE
@@ -19,28 +20,9 @@ var _blinky_coordinates : Vector2i
 
 func _ready() -> void:
 	_current_level = EventBus.current_level[EventBus.current_level_counter]
-	
 	_ghosts_array = find_children("*", "Ghost", false)
 	
-	scatter_chase_timer.start(EventBus.scatter_chase_timing[_current_level][0])
-	scatter_chase_timer.timeout.connect(func() -> void:
-		EventBus.scatter_chase_timing_counter += 1
-		
-		_current_state = EventBus.scatter_chase_timing_counter % 2
-		#print(_current_state, " = ", EventBus.scatter_chase_timing_counter)
-		for ghost in _ghosts_array:
-			if (
-				ghost.is_inside_the_ghost_house == false and 
-				(ghost.current_state == ghost.State.TARGETING or ghost.current_state == ghost.State.EATEN)
-			):
-				ghost.switch_direction()
-		var time : float = EventBus.scatter_chase_timing[_current_level][EventBus.scatter_chase_timing_counter]
-		if time != -1.0:
-			#print("Time: ", time)
-			scatter_chase_timer.start(time)
-		else:
-			scatter_chase_timer.stop()
-	)
+	_scatter_chase_behaviour()
 
 func _process(_delta: float) -> void:
 	for ghost in _ghosts_array:
@@ -80,6 +62,29 @@ func _assign_special_target(ghost: Node2D) -> void:
 			ghost_distance = floorf(ghost_distance)
 			print(ghost_distance)
 			ghost.target_coordinates = pacman_current_cell_coordinates if ghost_distance > 8.0 else ghost.scatter_coordinates
+
+func _scatter_chase_behaviour() -> void:
+	if EventBus.current_level_counter > 5:
+		_scatter_chase_timing = EventBus.scatter_chase_timing["level_5"]
+	else:
+		_scatter_chase_timing = EventBus.scatter_chase_timing[_current_level]
+	
+	scatter_chase_timer.start(_scatter_chase_timing[0])
+	scatter_chase_timer.timeout.connect(func() -> void:
+		_scatter_chase_timing_counter += 1
+		_current_state = _scatter_chase_timing_counter % 2
+		for ghost in _ghosts_array:
+			if (
+				ghost.is_inside_the_ghost_house == false and 
+				(ghost.current_state == ghost.State.TARGETING or ghost.current_state == ghost.State.EATEN)
+			):
+				ghost.switch_direction()
+		var time : float = _scatter_chase_timing[_scatter_chase_timing_counter]
+		if time != -1.0:
+			scatter_chase_timer.start(time)
+		else:
+			scatter_chase_timer.stop()
+	)
 
 func frightened() -> void:
 	for ghost in _ghosts_array:
