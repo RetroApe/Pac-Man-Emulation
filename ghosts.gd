99756f54e8772229dcc4662069a872859e24c236
Tomislav Šuto <113673335+RetroApe@users.noreply.github.com>
2025-05-23 12:17:52 +0200

    Added Exit Timer

--- ghosts.gd
+++ ghosts.gd
@@ -6,6 +6,9 @@ signal ghost_eaten
 signal ghost_eaten_but_make_pacman_visible
 
 @onready var global_count_label: Label = %GlobalCountLabel
+@onready var exit_timer: Timer = %ExitTimer
+@onready var exit_timer_label: Label = %ExitTimerLabel
+
 
 @onready var scatter_chase_timer: Timer = %ScatterChaseTimer
 var _scatter_chase_timing : Array
@@ -30,9 +33,48 @@ func _ready() -> void:
 	_current_level = GameState.current_level[GameState.current_level_counter]
 	_ghosts_array = find_children("*", "Ghost", false)
 	
+	_exit_timer_setup()
 	_scatter_chase_behaviour()
 
+func _scatter_chase_behaviour() -> void:
+	if GameState.current_level_counter > 5:
+		_scatter_chase_timing = GameState.scatter_chase_timing["level_5"]
+	else:
+		_scatter_chase_timing = GameState.scatter_chase_timing[_current_level]
+	
+	scatter_chase_timer.start(_scatter_chase_timing[0])
+	scatter_chase_timer.timeout.connect(func() -> void:
+		_scatter_chase_timing_counter += 1
+		_current_state = CHASE if _scatter_chase_timing_counter % 2 else SCATTER
+		for ghost in _ghosts_array:
+			if (
+				ghost.is_inside_the_ghost_house == false and 
+				(ghost.current_state == ghost.State.TARGETING or ghost.current_state == ghost.State.EATEN)
+			):
+				ghost.switch_direction()
+		var time : float = _scatter_chase_timing[_scatter_chase_timing_counter]
+		if time != -1.0:
+			scatter_chase_timer.start(time)
+		else:
+			scatter_chase_timer.stop()
+	)
+
+func _exit_timer_setup() -> void:
+	if GameState.current_level_counter < 5:
+		exit_timer.start(4.0)
+	else:
+		exit_timer.start(3.0)
+	
+	exit_timer.timeout.connect(func() -> void:
+		for ghost in _ghosts_array as Array[Ghost]:
+			if ghost.release == false:
+				ghost.release = true
+				break
+	)
+
 func _process(_delta: float) -> void:
+	exit_timer_label.text = "Exit Timer: " + str(exit_timer.time_left).pad_decimals(2)
+	
 	for ghost in _ghosts_array as Array[Ghost]:
 		if ghost.name == "Blinky":
 			_blinky_coordinates = ghost.current_cell_coordinates
@@ -72,29 +114,6 @@ func _assign_special_target(ghost: Node2D) -> void:
 			ghost_distance = floorf(ghost_distance)
 			ghost.target_coordinates = pacman_current_cell_coordinates if ghost_distance > 8.0 else ghost.scatter_coordinates
 
-func _scatter_chase_behaviour() -> void:
-	if GameState.current_level_counter > 5:
-		_scatter_chase_timing = GameState.scatter_chase_timing["level_5"]
-	else:
-		_scatter_chase_timing = GameState.scatter_chase_timing[_current_level]
-	
-	scatter_chase_timer.start(_scatter_chase_timing[0])
-	scatter_chase_timer.timeout.connect(func() -> void:
-		_scatter_chase_timing_counter += 1
-		_current_state = CHASE if _scatter_chase_timing_counter % 2 else SCATTER
-		for ghost in _ghosts_array:
-			if (
-				ghost.is_inside_the_ghost_house == false and 
-				(ghost.current_state == ghost.State.TARGETING or ghost.current_state == ghost.State.EATEN)
-			):
-				ghost.switch_direction()
-		var time : float = _scatter_chase_timing[_scatter_chase_timing_counter]
-		if time != -1.0:
-			scatter_chase_timer.start(time)
-		else:
-			scatter_chase_timer.stop()
-	)
-
 func frightened() -> void:
 	_ghost_eaten_counter = 0
 	for ghost in _ghosts_array:
@@ -104,9 +123,10 @@ func frightened() -> void:
 
 func set_dots(new_dots: int) -> void:
 	pacman_dots_eaten = new_dots
+	exit_timer.start()
 	if _ghost_dot_counter_active == true:
-		for ghost in _ghosts_array:
-			if ghost.personal_dot_count_reached == false:
+		for ghost in _ghosts_array as Array[Ghost]:
+			if ghost.personal_dot_count_reached == false and ghost.is_inside_the_ghost_house:
 				ghost = ghost as Ghost
 				ghost.personal_dot_counter += 1
 				break
@@ -125,6 +145,7 @@ func on_pacman_dead() -> void:
 	GameState.global_dot_counter_active = true
 	GameState.global_dot_count = 0
 	_ghost_dot_counter_active = false
+	exit_timer.stop()
 
 func _ghost_death_process(ghost : Ghost) -> void:
 	ghost.death()
@@ -140,6 +161,7 @@ func _ghost_death_process(ghost : Ghost) -> void:
 
 func on_game_start() -> void:
 	_is_pacman_dead = false
+	exit_timer.start()
 	for ghost in _ghosts_array as Array[Ghost]:
 		ghost.match_animation()
 
